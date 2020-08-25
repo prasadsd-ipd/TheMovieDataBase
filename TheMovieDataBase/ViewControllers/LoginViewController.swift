1b506cefae34007feee0a29d0bc7a9d26d60e36d
sdurgap <harman.durga_prasad@daimler.com>
2020-08-25 22:57:22 +0530

    Initial setup

---
+++ TheMovieDataBase/ViewControllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  TheMovieDataBase
+//
+//  Created by Durga Prasad, Sidde (623-Extern) on 25/08/20.
+//  Copyright © 2020 SDP. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
